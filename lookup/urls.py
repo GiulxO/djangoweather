5d3e16a1a56a97a4e46bcc80bca5e58983b8a5fb
Steven Bernis <stephtry8@gmail.com>
2021-06-27 13:21:41 +0200

    added changes to views.py

--- lookup/urls.py
+++ lookup/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path 
-from . import views
-urlpatterns = [
-    path('', views.home, name="home"), # taken from views.py the home function 
-    path('about', views.about, name="about"), 
 
+# This is my views.py file
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
+
+       
 ]
